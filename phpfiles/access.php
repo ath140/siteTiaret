009f0088b596d3ae6919fb78f5b587ac081ec29d
ath140 <athmohamed140gmail.com>
2024-12-09 09:13:59 +0100

    unite de tiaret premier pas

---
+++ phpfiles/access.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" />
+  <link rel="stylesheet" href="/css/access.css"/>
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="content">
+    <div class="form-titre">
+      <h3>CONNECTER</h3>
+    </div>
+    <form class="form-body" action="#" methode="POST">
+      <div class="input-group">
+        <i class="fa fa-envelope"></i></i>
+        <input type="text" placeholder="email" name="email">
+      </div>
+      <div class="input-group">
+        <i class="fa fa-key"></i></i>
+        <input type="text" placeholder="mot de passe" name="email">
+      </div>
+      <button type="submit" name="submit">connect</button>
+    </form>
+  </div>
+</body>
+
+</html>
